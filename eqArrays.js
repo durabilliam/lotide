16e62ef729ef149c37cf0d489abcaf45f0811aa2
William Perry <durabilliam@gamil.com>
2021-03-30 20:25:42 -0230

    updates

--- eqArrays.js
+++ eqArrays.js
@@ -13,3 +13,22 @@ const assertEqual = function(actual, expected) {
     console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let out = [];
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i] && (typeof(arrayOne[i]) === "string" || typeof(arrayTwo[i]) === "string")) {
+      out.push("true");
+    } else if (arrayOne[i] === arrayTwo[i] && (typeof(arrayOne[i]) === "number" || typeof(arrayTwo[i]) === "number")) {
+      out.push("true");
+    } else out.push("false");
+  }
+  if (out.includes("false")) {
+    return false;
+  } else return true;
+};
+
+(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true));
+(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
+(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true));
