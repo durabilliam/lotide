7290b1974209e2a3f81338ed47b84993a56516c3
William Perry <durabilliam@gamil.com>
2021-04-01 20:17:58 -0230

    updates

--- countLetters.js
+++ countLetters.js
@@ -1,35 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: \"${actual}\" === \"${expected}\"`);
-  } else if (actual !== expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: \"${actual}\" !== \"${expected}\"`);
-  } else if (actual === expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const countLetters = function (inputString){
+const countLetters = function(inputString) {
   const obCountOut = {};
   let inputStringNoSpaces = inputString.replace(/ /g, "");
-  console.log(inputStringNoSpaces)
-  ////read the string and tally each occurance
-  ///output is an object
-  for (let i = 0; i < inputStringNoSpaces.length; i++){
+  for (let i = 0; i < inputStringNoSpaces.length; i++) {
     let count = 0;
-    for (let j =0; j < inputStringNoSpaces.length; j++){
-      if (inputStringNoSpaces[i] === inputStringNoSpaces[j]){
-        count++
+    for (let j = 0; j < inputStringNoSpaces.length; j++) {
+      if (inputStringNoSpaces[i] === inputStringNoSpaces[j]) {
+        count++;
         obCountOut[inputStringNoSpaces[i]] = count;
       }
     }
   } return obCountOut;
 };
 
-console.log(countLetters("the the the the man is grumpy"))
-console.log(countLetters("This IS A fun Time to Cry and Poop"))
+console.log(countLetters("the the the the man is grumpy"));
--- findKey.js
+++ findKey.js
@@ -1,12 +1,13 @@
-const findKey = function (object, callback){
-  for (let keys in object){
-    const star = object[keys]
-    if (callback(star)){
-     return keys
+const findKey = function(object, callback) {
+  for (let keys in object) {
+    const star = object[keys];
+    if (callback(star)) {
+      return keys;
     }
   }
-  return undefined
-}
+  return undefined;
+};
+
 const objectList = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
@@ -14,6 +15,6 @@ const objectList = {
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}
+};
 
 console.log("hgdjge", findKey(objectList, x => x.stars === 2));
