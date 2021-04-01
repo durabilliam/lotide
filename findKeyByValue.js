d7daca00e3fa62c281252dd00317f14aa141c9e0
William Perry <durabilliam@gamil.com>
2021-03-31 21:36:51 -0230

    findbykey

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
+    console.log(`\uD83D\uDC7D Assertion Passed: \"${actual}\" === \"${expected}\"`);
+  } else if (actual !== expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
+    console.log(`\uD83E\uDD2E Assertion Failed: \"${actual}\" !== \"${expected}\"`);
+  } else if (actual === expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
+    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
+    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
+    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
+    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(genres, shows) {
+  const keys = Object.keys(genres);
+  for (let key of keys) {
+    if (genres[(key)] === shows) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "The Wire");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
