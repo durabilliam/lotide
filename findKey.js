e26a462c835ffae5f34e482c68b7db8f8acbd072
William Perry <durabilliam@gamil.com>
2021-04-01 20:00:16 -0230

    added findkey

---
+++ findKey.js
@@ -0,0 +1,19 @@
+const findKey = function (object, callback){
+  for (let keys in object){
+    const star = object[keys]
+    if (callback(star)){
+     return keys
+    }
+  }
+  return undefined
+}
+const objectList = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
+
+console.log("hgdjge", findKey(objectList, x => x.stars === 2));
