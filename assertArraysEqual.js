9ff2547c391dc6ab9f41900e815ef36611d521cc
William Perry <durabilliam@gamil.com>
2021-04-05 09:11:35 -0230

    added modular

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: \"${actual}\" === \"${expected}\"`);
-  } else if (actual !== expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: \"${actual}\" !== \"${expected}\"`);
-  } else if (actual === expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  } return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arraysOne, arraysTwo) {
   if (eqArrays(arraysOne, arraysTwo) === true) {
@@ -31,7 +6,5 @@ const assertArraysEqual = function(arraysOne, arraysTwo) {
   } else console.log(`\uD83E\uDD2E Assertion Failed: ${arraysOne} !== ${arraysTwo}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
+
