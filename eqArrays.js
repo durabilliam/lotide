5aeefde49366f165118bb8df9fd7a9d78c949384
William Perry <durabilliam@gamil.com>
2021-04-05 09:04:58 -0230

    made modular

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: \"${actual}\" === \"${expected}\"`);
-  } else if (actual !== expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: \"${actual}\" !== \"${expected}\"`);
-  } else if (actual === expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
     return false;
@@ -25,7 +9,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   } return true;
 };
 
-(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true));
-(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
-(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true));
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,29 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: \"${actual}\" === \"${expected}\"`);
-  } else if (actual !== expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: \"${actual}\" !== \"${expected}\"`);
-  } else if (actual === expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(input) {
   let output = [];
   for (let i = 0; i < (input.length - 1); i++) {
     output[i] = input[i + 1];
   }
-  console.log(input);
-  console.log(output);
   return output;
 };
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
