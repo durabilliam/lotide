de59b3dec9726fc8418c42be99e73ac00aa9e73f
William Perry <durabilliam@gamil.com>
2021-04-05 14:41:32 -0230

    added mocha/chai to middle and tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,18 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
-console.log(middle([1, 2, 3, 4, 5]));
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+describe("#middle", () => {
 
-console.log(middle([1, 2]));
-assertArraysEqual(middle([1, 2]), []);
+  it("returns middle value in an array of numbers[1, 2, 3, 4, 5]", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+  });
 
-console.log(middle(["one", "two", "three", "four", "five", "six"]));
-assertArraysEqual(middle(["one", "two", "three", "four", "five", "six"]), ["three", "four"]);
+  it("returns empty array if input is 2 elements or less[1, 2]", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2]), []));
+  });
+
+  it("returns middle two elements if input has even number of elements['one', 'two', 'three', 'four', 'five', 'six'] ", () => {
+    assert.strictEqual(assertArraysEqual(middle(['one', 'two', 'three', 'four', 'five', 'six']), ['three', 'four']));
+  });
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,22 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#head", () => {
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+
+  it("returns the orginal array length untouched for ['Hello', 'Lighthouse', 'Labs']", () => {
+    let input = ['Hello', 'Lighthouse', 'Labs'];
+    let result = tail(['Hello', 'Lighthouse', 'Labs'])
+    assert.lengthOf(input, 3);
+  });
+
+  it("returns the output array with a new length ['Hello', 'Lighthouse', 'Labs'] of 2" , () => {
+    let input = ['Hello', 'Lighthouse', 'Labs'];
+    let result = tail(['Hello', 'Lighthouse', 'Labs'])
+    assert.lengthOf(result, 2);
+  });
+});
+
