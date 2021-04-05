890f570791318f741187855e98e40dfdebaa7cbf
William Perry <durabilliam@gamil.com>
2021-04-05 08:26:01 -0230

    updated

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,13 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
+  if (actual === expected) {
     console.log(`\uD83D\uDC7D Assertion Passed: \"${actual}\" === \"${expected}\"`);
-  } else if (actual !== expected && (typeof(actual) === "string" || typeof(expected) === "string")) {
+  } else if (actual !== expected) {
     console.log(`\uD83E\uDD2E Assertion Failed: \"${actual}\" !== \"${expected}\"`);
-  } else if (actual === expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "number" || typeof(expected) === "number")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
-    console.log(`\uD83D\uDC7D Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected && (typeof(actual) === "bolean" || typeof(expected) === "boolean")) {
-    console.log(`\uD83E\uDD2E Assertion Failed: ${actual} !== ${expected}`);
-  }
+  } 
 };
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 9);
+assertEqual(1, "1");
